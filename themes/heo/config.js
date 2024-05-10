0c6000dd218300aa97f4cf35fb850b316ac01108
Wick-1998 <127816138+Wick-1998@users.noreply.github.com>
2024-05-10 14:41:24 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -16,8 +16,8 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '践行者-行歌',
-  HEO_HERO_TITLE_2: 'WEB-3.0',
-  HEO_HERO_TITLE_3: '----',
+  HEO_HERO_TITLE_2: '',
+  HEO_HERO_TITLE_3: 'WEB-3.0',
   HEO_HERO_TITLE_4: '武林秘籍',
   HEO_HERO_TITLE_5: '行歌-推荐的宝藏内容',
   HEO_HERO_TITLE_LINK: '',
