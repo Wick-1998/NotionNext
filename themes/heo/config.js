d3d8d17a72f19b01c9f5dad316073c9869549fa7
Wick-1998 <127816138+Wick-1998@users.noreply.github.com>
2024-10-10 15:21:44 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,7 +18,7 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_1: '分享交易',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
   HEO_HERO_TITLE_4: '新版上线',
@@ -28,9 +28,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '行情分析', url: '/tag/行情分析' },
+  HEO_HERO_CATEGORY_2: { title: '炒币故事', url: '/tag/牛人牛事' },
+  HEO_HERO_CATEGORY_3: { title: '新人入门', url: '/tag/新人入门' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -40,10 +40,10 @@ const CONFIG = {
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
+    '🔍 WEB3坚定拥抱者',
+    '🤝 阿尔法猎手',
     '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
+    '🏠 低抛高接技术派',
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
